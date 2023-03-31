707ae7d21f0eb3a6a45acfa86525d7c3f046ea28
mamylinx <mamy.else@gmail.com>
2023-03-31 11:18:13 +0300

    Fix space issue

--- static/js/datatables-renderer.js
+++ static/js/datatables-renderer.js
@@ -3,13 +3,11 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
         render: function (context, element, attributes) {
             var renderer = new DatatablesRenderer.Renderer();
             element.innerHTML = renderer.getHtml(element.innerHTML, attributes, context);
-
-            // Strange behaviour from IE. 
             // It comes here 2 times per row, so I have to stop rendering a second time to avoid desctruction of the rendering
             if (context != "timeslider" && element.innerHTML && element.innerHTML.indexOf("payload") != 2) {
                 return;
             }
-
+            renderer = new DatatablesRenderer.Renderer();
             if (context == "timeslider") {
                 var regex1 = new RegExp('(^\<span\ class=""\>)', 'i');
                 var regex2 = new RegExp('(\<\/span\>)$', 'i');
@@ -18,7 +16,8 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
                     .replace(regex2, '');
             } else if (context == "export") {
                 code = element.text;
-            } else code = element.innerHTML;
+            } else if (element.innerHTML) code = element.innerHTML;
+            else code = element.innerHTML;
 
             if (context == "export") {
                 // For export, I need to send back the formatted text
@@ -43,13 +42,10 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
             };
         },
         buildTabularData: function (tblJSONObj, tblProperties, renderingContext) {
-            // var tblId = tblJSONObj.tblId;
             var tblClass = tblJSONObj.tblClass;
-            // var tdClass = tblJSONObj.tdClass;
             var trClass = tblJSONObj.trClass;
-            // var payload = tblJSONObj.payload;
             var rowAttrs, singleRowAttrs, cellAttrs, colAttrs;
-            var tblWidth, tblHeight, tblBorderWidth, tblBorderColor;
+            var tblWidth, tblHeight;
 
             if (!tblProperties || tblProperties.length == 0) {
                 tblProperties = this.createDefaultTblProperties();
@@ -61,32 +57,23 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
             colAttrs = tblProperties.colAttrs;
             tblWidth = tblProperties.width || "100";
             tblHeight = tblProperties.height || "15";
-            tblBorderWidth = tblProperties.borderWidth || 0;
-            tblBorderColor = tblProperties.borderColor || "grey";
-
 
             var authors = tblProperties.authors;
             var printViewTBlStyles = "table-layout:fixed !important;border-collapse:collapse!important;";
             if (renderingContext == "export") printViewTBlStyles += "margin-bottom: -17px;";
             var printViewTblTDStyles = "padding: 5px 7px;word-wrap: break-word!important;"
-            var bordersBottom = "border-bottom:" + tblBorderWidth + "px solid " + tblBorderColor;
-            var bordersTop = "border-top:" + tblBorderWidth + "px solid " + tblBorderColor;
+            var bordersBottom = "border-bottom: 1px solid grey";
+            var bordersTop = "border-top: 1px solid grey";
             var rowVAlign = rowAttrs.rowVAlign || "left";
             var rows = tblJSONObj.payload;
-            var evenRowBgColor = rowAttrs.evenBgColor || "#FFFFFF";
-            var oddRowBgColor = rowAttrs.oddBgColor || null;
 
             var htmlTbl = "<table class='" + tblClass + "' style='" + printViewTBlStyles + "background-color:white;width:" + tblWidth + "%!important;height:" + tblHeight + "px!important;'><tbody>";
 
             // the tables contains only one row, no need to do a FOR
             for (var j = 0, rl = rows.length; j < rl; j++) {
                 var tds = rows[j];
-                //console.log("draw table", tds, tblProperties);
-                var rowBgColor = oddRowBgColor;
-                if (!rowBgColor) {
-                    rowBgColor = evenRowBgColor;
-                }
-                htmlTbl += "<tr style='vertical-align:" + rowVAlign + ";background-color:" + rowBgColor + "; " + bordersBottom + "!important;";
+            
+                htmlTbl += "<tr style='vertical-align:" + rowVAlign + "; " + bordersBottom + "!important;";
                 if (tblProperties.isFirstRow) htmlTbl += " " + bordersTop + "!important;";
                 htmlTbl += "' class='" + trClass + "'>";
                 var preHeader = j == 0 ? "{\"payload\":[[\"" : "";
@@ -95,21 +82,19 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
                 for (var i = 0, tl = tds.length; i < tl; i++) {
                     var cellAttr = typeof (cellAttrs[j]) == 'undefined' || cellAttrs[j] == null ? null : cellAttrs[j][i];
                     var cellStyles = this.getCellAttrs(singleRowAttr, cellAttr, colAttrs[i], authors, i, j);
-
-                    var borderTop = "";
-                    if (tblBorderWidth == 0) {
-                        borderTop = " border-top: 0px solid white !important;";
-                    }
                     var colVAlign = typeof (colAttrs[i]) == 'undefined' || colAttrs[i] == null ? "" : "align='" + colAttrs[i].colVAlign + "'";
                     var quoteAndComma = "\",\"";
                     var delimCell = "<td class='regex-delete' name='delimCell' id='" + "' class='hide-el overhead' style='display:none;'>" + quoteAndComma + "</td>";
                     var lastCellBorder = "";
                     if (i == tl - 1) {
                         delimCell = "";
-                        lastCellBorder = "border-right:" + tblBorderWidth + "px solid " + tblBorderColor + "!important;";
+                        lastCellBorder = "border-right: 1px solid grey !important;";
                         quoteAndComma = "";
                     }
+
                     tds[i] = this.setLinks(tds[i]);
+                    tds[i] = this.handleSpace(tds[i]);
+                
                     if (tds[i].indexOf('/r/n') != -1) {
                         let cellsWithBr = "";
                         var tdText = tds[i].split('/r/n');
@@ -118,11 +103,10 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
                                 cellsWithBr += tdText[k] + "<label value='tblBreak' class='hide-el' style='display:none;'>/r/n</label><label class='tblBreak' style='display:block;'></label>";
                             } else cellsWithBr += tdText[k];
                         }
-                        htmlTbl += "<td  name='tData' " + colVAlign + " style='" + printViewTblTDStyles + cellStyles + " border-left:" +
-                            tblBorderWidth + "px solid " + tblBorderColor + ";" + borderTop + lastCellBorder + "' >" + cellsWithBr +
+                        htmlTbl += "<td  name='tData' " + colVAlign + " style='" + printViewTblTDStyles + cellStyles + " border-left: 1px solid grey;" + lastCellBorder + "' >" + cellsWithBr +
                             "<br value='tblBreak'></td>" + delimCell;
                     } else {
-                        htmlTbl += "<td name='tData' " + colVAlign + " style='" + printViewTblTDStyles + cellStyles + lastCellBorder + " border-left:" + tblBorderWidth + "px solid " + tblBorderColor + ";" + borderTop + "' >" + tds[i] + "" + "<br value='tblBreak'></td>" + delimCell
+                        htmlTbl += "<td name='tData' " + colVAlign + " style='" + printViewTblTDStyles + cellStyles + lastCellBorder + " border-left: 1px solid grey' >" + tds[i] + "" + "<br value='tblBreak'></td>" + delimCell
                     }
                 }
                 var bracketAndcomma = "\"]],\"tblId\":\"1\",\"tblClass\":\"data-tables\", \"tblProperties\":" + JSON.stringify(tblProperties) + "}";
@@ -136,22 +120,8 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
             var attrsJSO = {};
             var colWidth = typeof (colAttr) == 'undefined' || colAttr == null ? "" : colAttr.width || "";
             attrsJSO['width'] = colWidth + 'px';
-            var cellBgColor = "";
-            //row highlight
-            if (typeof (singleRowAttr) != 'undefined' && singleRowAttr != null) {
-                let bgColor = singleRowAttr.bgColor;
-                if (typeof (bgColor) != 'undefined' && bgColor != null && bgColor != '#FFFFFF') {
-                    cellBgColor = bgColor;
-                }
-            }
-            //col highlight
-            if (typeof (colAttr) != 'undefined' && colAttr != null) {
-                let bgColor = colAttr.bgColor;
-                if (typeof (bgColor) != 'undefined' && bgColor != null && bgColor != '#FFFFFF') {
-                    cellBgColor = bgColor;
-                }
-            }
-            cellBgColor = typeof (cellAttr) == 'undefined' || cellAttr == null ? cellBgColor : cellAttr.bgColor || cellBgColor;
+            var cellBgColor = "#FFFFFF";
+           
             attrsJSO['background-color'] = cellBgColor;
             var cellHeight = typeof (cellAttr) == 'undefined' || cellAttr == null ? "" : cellAttr.height || "";
             attrsJSO['height'] = cellHeight + 'px';
@@ -185,6 +155,11 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
 
             return string;
         },
+
+        handleSpace: function(data) {
+            data = data.replace(/ /g, '\u00a0');
+            return data;
+        },
         setLinks: function (data) {
             data = data.replace(/(https?:\/\/[^\s]+)/ig, "<a href='\$1' target='blank'>\$1</a>");
             return data;
@@ -192,6 +167,7 @@ if (typeof (DatatablesRenderer) == 'undefined') var DatatablesRenderer = functio
         getHtml: function (code, attributes, renderingContext) {
             var JSONCode = "";
             var html = "";
+            // code = code.replace(/<a.*>.*?<\/a>/ig, '').replace(/https?\:\/\//ig, '');
             try {
                 JSONCode = JSON.parse(code);
                 tblProperties = JSONCode.tblProperties;
